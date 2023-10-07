2c1f0e2b3710fc5a99ca553c3f46439112c6694f
syntway <4272280+syntway@users.noreply.github.com>
2023-10-07 07:43:26 +0100

    Fixed continue.py

--- projects/continue/continue.py
+++ projects/continue/continue.py
@@ -1,3 +1,20 @@
+"""
+Run a prompt with the GPT2 smallest model.
+
+Other available models:
+'gpt2': 124M params
+'gpt2-medium': 350M params
+'gpt2-large': 774M params
+'gpt2-xl': 1558M params
+
+To use another model, change init='...' below.
+
+Another way of running, directly from commad line:
+
+python -m gptbench.run -init=gpt2 -mode=prompt
+
+"""
+
 import sys
 
 from gptbench import empty_config, config_run
@@ -7,6 +24,7 @@ from gptbench import empty_config, config_run
 if __name__ == '__main__':
 
     c = empty_config()
+    c.set(init='gpt2', mode='prompt')
 
     # train
 
